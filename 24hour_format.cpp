b6440521c06ed0e1b507acd908c25de42523c456
evosevil <64919642+evosevil@users.noreply.github.com>
2021-02-02 12:17:42 +0530

    Final Commit
    All changes have been made.

--- 24hour_format.cpp
+++ 24hour_format.cpp
@@ -4,23 +4,29 @@ using namespace std;
 
 int main()
 {
-    string s = "07:05:45PM";
-    int x = 0;
+    string s;
+    getline(cin,s);
+    int r = 0,x=0;
     string hour;
+    
+    hour = s.substr(0, 2);          // 12
+    stringstream hrs(hour);         // to convert int into str
+    hrs>>x;                         // assigning the value of hrs to x
 
-    // cout<<"Enter time: ";
-    // getline(cin,s);
-
-    if (s.find("PM") == 8)
+    if (s.find("PM") == 8 && x!=12)
     {
         //hrs section 
-        hour = s.substr(0, 2);
-        stringstream hrs(hour);
-        hrs>>x;
-        x=x+12;
-        string output = to_string(x) + s.substr(2,6);
+        r=x+12;
+        string output = to_string(r) + s.substr(2,6);
         cout<<output;
 
+    }
+    else if((x==12) && (s.substr(8,10)=="AM")){
+        cout<<"00"+ s.substr(2,6);
+    }
+    else{
+        string op = s.substr(0,8);
+        cout<<op;
     }
     return 0;
 }
